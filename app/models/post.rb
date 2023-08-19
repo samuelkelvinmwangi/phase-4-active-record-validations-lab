570b1fae78979c17d8deb5e3332b6a5f8f7f4548
samuel <samuelkelvinmwangi@gmail.com>
2023-08-19 18:26:43 +0300

    adding validations post and author classes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait_title
+
+  private
+
+  def clickbait_title
+    clickbait_phrases = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    
+    if title.present? && clickbait_phrases.none? { |phrase| title.include?(phrase) }
+      errors.add(:title, "must be sufficiently clickbait-y")
+    end
+  end
 end
